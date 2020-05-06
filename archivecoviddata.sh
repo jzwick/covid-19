e0631ea82b159b9507f04617b18747a872f5913e
Justine Zwicker <justinezed@gmail.com>
2020-05-05 21:53:40 -0400

    new canada covid nb

--- archivecoviddata.sh
+++ archivecoviddata.sh
@@ -4,3 +4,7 @@ cd ~/Documents/coronavirus-data
 git fetch upstream
 git rebase upstream/master
 python ../Python\ Scripts/archive_covid_data.py ~/Documents/coronavirus-data/ ~/Documents/COVID/covid-data-archive/ False
+
+cd ~/Documents/Covid19Canada
+git fetch upstream
+git rebase upstream/master
